b2a833e016475b8814cf07c1b23925d40bc860e1
Clinton Biswas <ponti.clinton@gmail.com>
2023-12-12 13:30:46 +0600

    second-commit

--- sanatan_society/settings.py
+++ sanatan_society/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 
 # email Backend
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
